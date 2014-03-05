198970a4cec975911eee365e7f587e1913633679
David Moebius <dmoebius@gmail.com>
2014-03-05 17:26:13 +0100

    [task] added methods to do different tasks

--- stress-tests/src/main/java/org/osiam/tests/stress/OsiamContext.java
+++ stress-tests/src/main/java/org/osiam/tests/stress/OsiamContext.java
@@ -1,9 +1,13 @@
 package org.osiam.tests.stress;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.osiam.client.connector.OsiamConnector;
 import org.osiam.client.oauth.AccessToken;
 import org.osiam.client.oauth.GrantType;
 import org.osiam.client.oauth.Scope;
+import org.osiam.resources.scim.User;
 
 public class OsiamContext {
     private static OsiamContext mySingelton = null;
@@ -11,9 +15,9 @@ public class OsiamContext {
     private static final String RESOURCE_ENDPOINT_ADDRESS = "http://localhost:8180/osiam-resource-server";
     private static final String CLIENT_ID = "example-client";
     private static final String CLIENT_SECRET = "secret";
-    
-    private OsiamConnector oConnector = null;
+
     private AccessToken accessToken = null;
+    private List<User> users = new ArrayList<User>();
 
     private OsiamContext() {
     }
@@ -26,21 +30,19 @@ public class OsiamContext {
     }
 
     public OsiamConnector getConnector() {
-        if (oConnector == null) {
-            OsiamConnector.Builder oConBuilder = new OsiamConnector.Builder().
-                    setAuthServiceEndpoint(AUTH_ENDPOINT_ADDRESS).
-                    setResourceEndpoint(RESOURCE_ENDPOINT_ADDRESS).
-                    setClientId(CLIENT_ID).
-                    setClientSecret(CLIENT_SECRET).
-                    setGrantType(GrantType.RESOURCE_OWNER_PASSWORD_CREDENTIALS).
-                    setUserName("marissa").
-                    setPassword("koala").
-                    setScope(Scope.ALL);
-            oConnector = oConBuilder.build();
-        }
+        OsiamConnector.Builder oConBuilder = new OsiamConnector.Builder().
+                setAuthServiceEndpoint(AUTH_ENDPOINT_ADDRESS).
+                setResourceEndpoint(RESOURCE_ENDPOINT_ADDRESS).
+                setClientId(CLIENT_ID).
+                setClientSecret(CLIENT_SECRET).
+                setGrantType(GrantType.RESOURCE_OWNER_PASSWORD_CREDENTIALS).
+                setUserName("marissa").
+                setPassword("koala").
+                setScope(Scope.ALL);
+        OsiamConnector oConnector = oConBuilder.build();
         return oConnector;
     }
-    
+
     public AccessToken getValidAccessToken() {
         if (accessToken == null)
         {
@@ -50,5 +52,20 @@ public class OsiamContext {
             getConnector().refreshAccessToken(accessToken, Scope.ALL);
         }
         return accessToken;
+    }
+
+    public void setListOfUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public String retrieveSingleUserId() {
+        String userId = null;
+        if (users.size() > 0) {
+            int randomPosition = (int) (Math.random() * users.size() + 1);
+            User user = users.get(randomPosition);
+            userId = user.getId();
+            users.remove(randomPosition);
+        }
+        return userId;
     }
 }
--- stress-tests/src/main/java/org/osiam/tests/stress/RequesterJob.java
+++ stress-tests/src/main/java/org/osiam/tests/stress/RequesterJob.java
@@ -1,26 +1,49 @@
 package org.osiam.tests.stress;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.osiam.resources.scim.Address;
+import org.osiam.resources.scim.Email;
+import org.osiam.resources.scim.Entitlement;
+import org.osiam.resources.scim.Extension;
+import org.osiam.resources.scim.Im;
+import org.osiam.resources.scim.Name;
+import org.osiam.resources.scim.PhoneNumber;
+import org.osiam.resources.scim.Photo;
+import org.osiam.resources.scim.Role;
+import org.osiam.resources.scim.SCIMSearchResult;
+import org.osiam.resources.scim.UpdateUser;
+import org.osiam.resources.scim.User;
+import org.osiam.resources.scim.X509Certificate;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import com.google.common.base.Strings;
+
 public class RequesterJob implements Job {
 
-    private OsiamContext osiamValues;
+    private OsiamContext osiamContext;
+    private String jobName;
+    private static final String EXTENSION_URN = "com.osiam.stress.test";
 
     public RequesterJob() {
-        osiamValues = OsiamContext.getInstance();
+        osiamContext = OsiamContext.getInstance();
     }
 
     public void execute(JobExecutionContext context) throws JobExecutionException {
+        jobName = context.getJobDetail().getKey().getName();
         try {
-            System.out.println(osiamValues.getValidAccessToken() == null);
             int i = (int) (Math.random() * 30 + 1);
 
             if (i > 0 && i <= 10) {
                 createNewUser();
-            }
-            else if (i > 10 && i <= 18) {
+            } else if (i > 10 && i <= 18) {
                 searchUser();
             } else if (i > 18 && i <= 26) {
                 getUser();
@@ -28,37 +51,202 @@ public class RequesterJob implements Job {
                 updateUser();
             } else if (i > 28 && i <= 29) {
                 replaceUser();
-            } else {
+            } else if(i == 30){
                 deleteUser();
             }
 
         } catch (Throwable e) {
-            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+            System.out.println("Error at Job " + jobName + ": " + e.getMessage());
         }
     }
 
     private void createNewUser() {
-        System.out.println("Creating a new User");
+        System.out.println(jobName + ": Creating a new User");
+
+        User user = getRandomUser();
+        osiamContext.getConnector().createUser(user, osiamContext.getValidAccessToken());
+    }
+
+    private User getRandomUser() {
+        List<Address> addresses = new ArrayList<Address>();
+        addresses.add(getRandomAddress());
+
+        List<Email> emails = new ArrayList<Email>();
+        emails.add(getRandomEmail());
+
+        List<Entitlement> entitlements = new ArrayList<Entitlement>();
+        entitlements.add(getRandomEntitilement());
+
+        List<Im> ims = new ArrayList<Im>();
+        ims.add(getRandomIm());
+
+        Name name = getRandomName();
+
+        List<PhoneNumber> phoneNumbers = new ArrayList<PhoneNumber>();
+        phoneNumbers.add(getRandomPhoneNumber());
+
+        List<Photo> photos = new ArrayList<Photo>();
+        photos.add(getRandomPhoto());
+
+        List<Role> roles = new ArrayList<Role>();
+        roles.add(getRandomRole());
+
+        List<X509Certificate> x509Certificates = new ArrayList<X509Certificate>();
+        x509Certificates.add(getRandomX509Certificate());
+
+        Extension extension = getRandomExtension();
+
+        return new User.Builder(UUID.randomUUID().toString())
+                .setActive(true)
+                .setAddresses(addresses)
+                .setDisplayName("displayName" + getRandomNumber())
+                .setEmails(emails)
+                .setEntitlements(entitlements)
+                .setExternalId(UUID.randomUUID().toString())
+                .setIms(ims)
+                .setLocale("de_DE")
+                .setName(name)
+                .setNickName("nickname" + getRandomNumber())
+                .setPassword("password" + getRandomNumber())
+                .setPhoneNumbers(phoneNumbers)
+                .setPhotos(photos)
+                .setPreferredLanguage("german")
+                .setProfileUrl("/user/username")
+                .setRoles(roles)
+                .setTimezone("DE")
+                .setTitle("title" + getRandomNumber())
+                .setX509Certificates(x509Certificates)
+                // .addExtension(extension) TODO extension doesn't exist at the moment
+                .build();
+    }
+
+    private Address getRandomAddress() {
+        Address address = new Address.Builder().setCountry("USA")
+                .setFormatted("formattedAddress").setLocality("Houston")
+                .setPostalCode("ab57" + getRandomNumber()).setPrimary(false).setRegion("Texas")
+                .setStreetAddress("Main Street. " + getRandomNumber()).setType(Address.Type.HOME)
+                .build();
+
+        return address;
+    }
+
+    private Email getRandomEmail() {
+        Email email = new Email.Builder().setPrimary(true)
+                .setValue("my" + getRandomNumber() + "@mail.com").setType(Email.Type.HOME).build();
+        return email;
+    }
+
+    private Entitlement getRandomEntitilement() {
+        Entitlement entitlement = new Entitlement.Builder().setPrimary(true)
+                .setType(new Entitlement.Type("not irrelevant"))
+                .setValue("some entitlement" + getRandomNumber()).build();
+        return entitlement;
+    }
+
+    private Im getRandomIm() {
+        Im im = new Im.Builder().setPrimary(true).setType(Im.Type.GTALK)
+                .setValue("gtalk" + getRandomNumber()).build();
+        return im;
+    }
+
+    private Name getRandomName() {
+        Name name = new Name.Builder().setFamilyName("Simpson")
+                .setFormatted("formatted" + getRandomNumber()).setGivenName("Homer")
+                .setHonorificPrefix("Dr.").setHonorificSuffix("Mr.")
+                .setMiddleName("J").build();
+        return name;
+    }
+
+    private PhoneNumber getRandomPhoneNumber() {
+        PhoneNumber phoneNumber = new PhoneNumber.Builder().setPrimary(true)
+                .setType(PhoneNumber.Type.WORK)
+                .setValue("00" + getRandomNumber() + getRandomNumber() + getRandomNumber()).build();
+        return phoneNumber;
+    }
+
+    private Photo getRandomPhoto() {
+        Photo photo = new Photo.Builder().setPrimary(true)
+                .setType(Photo.Type.PHOTO).setValue("username" + getRandomNumber() + ".jpg").build();
+        return photo;
+    }
+
+    private Role getRandomRole() {
+        Role role = new Role.Builder().setPrimary(true).setValue("user_role" + getRandomNumber())
+                .build();
+        return role;
+    }
+
+    private X509Certificate getRandomX509Certificate() {
+        X509Certificate x509Certificate = new X509Certificate.Builder()
+                .setPrimary(true).setValue("x509Certificat" + getRandomNumber()).build();
+        return x509Certificate;
+    }
+
+    private Extension getRandomExtension() {
+        Extension extension = new Extension(EXTENSION_URN);
+        extension.addOrUpdateField("gender", "female");
+        extension.addOrUpdateField("age", new BigInteger("" + getRandomNumber()));
+        return extension;
+    }
+
+    private int getRandomNumber() {
+        return (int) (Math.random() * 99 + 1);
     }
 
     private void updateUser() {
-        System.out.println("Updating a User");
+        System.out.println(jobName + ": Updating a User");
+        String userId = osiamContext.retrieveSingleUserId();
+        if (!Strings.isNullOrEmpty(userId)) {
+            UpdateUser updateUser = new UpdateUser.Builder().updateExternalId(UUID.randomUUID().toString()).build();
+            osiamContext.getConnector().updateUser(userId, updateUser, osiamContext.getValidAccessToken());
+        }
     }
 
     private void replaceUser() {
-        System.out.println("Replace a User");
+        System.out.println(jobName + ": Replace a User");
+        String userId = osiamContext.retrieveSingleUserId();
+        if (!Strings.isNullOrEmpty(userId)) {
+            User replaceUser = getRandomUser();
+            osiamContext.getConnector().replaceUser(userId, replaceUser, osiamContext.getValidAccessToken());
+        }
     }
 
     private void deleteUser() {
-        System.out.println("deleting a User");
+        System.out.println(jobName + ": deleting a User");
+        System.out.println(jobName + ": get a Users");
+        String userId = osiamContext.retrieveSingleUserId();
+        if (!Strings.isNullOrEmpty(userId)) {
+            osiamContext.getConnector().deleteUser(userId, osiamContext.getValidAccessToken());
+        }
     }
 
-    private void searchUser() {
-        System.out.println("searching for Users");
+    private void searchUser() throws UnsupportedEncodingException {
+        System.out.println(jobName + ": searching for Users");
+        String query = getCompletUserQueryString();
+        SCIMSearchResult<User> queryResult = osiamContext.getConnector().searchUsers("filter=" + query,
+                osiamContext.getValidAccessToken());
+        if (queryResult.getTotalResults() == 0) {
+            queryResult = osiamContext.getConnector().searchUsers("filter=",
+                    osiamContext.getValidAccessToken());
+        }
+        osiamContext.setListOfUsers(queryResult.getResources());
+    }
+
+    private String getCompletUserQueryString() throws UnsupportedEncodingException {
+        String encoded = null;
+        encoded = URLEncoder.encode("active eq \"true\""
+                + " and addresses.postalCode co \"" + getRandomNumber() + "\""
+                + " and addresses.primary eq \"true\"", "UTF-8");
+        return encoded;
     }
 
     private void getUser() {
-        System.out.println("get a Users");
+        System.out.println(jobName + ": get a Users");
+        String userId = osiamContext.retrieveSingleUserId();
+        if (!Strings.isNullOrEmpty(userId)) {
+            osiamContext.getConnector().getUser(userId, osiamContext.getValidAccessToken());
+        }
     }
 
 }
